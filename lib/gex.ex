12c5faec3a3f6f17b51496a35c45b5f6a7c950d9
Philihp Busby <philihp@gmail.com>
2020-01-06 17:13:33 +0000

    remove boilerplate

--- lib/gex.ex
+++ lib/gex.ex
@@ -1,18 +1,2 @@
 defmodule Gex do
-  @moduledoc """
-  Documentation for Gex.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Gex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
